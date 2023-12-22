9dfdc9fb7bd5689b5d809f2c074fabf9603a9566
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2023-12-22 12:57:39 +0530

    feat : integrating firebase for hosting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
